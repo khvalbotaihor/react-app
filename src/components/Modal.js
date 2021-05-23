9a565e56af6b408a87cc8605078fbaa343402a91
Ihor Khvalbota Software Engineer <ihorkhvalbota@gmail.com>
2021-05-23 15:09:51 +0300

    up

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,7 +1,7 @@
 function Modal() {
     return (
         <div className='modal'>
-            <p>Ae you sure?</p>
+            <p>Are you sure?</p>
             <button className='btn btn--alt'>Cancel</button>
             <button className='btn'>Confirm</button>
         </div>)
--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,19 +1,25 @@
 import {useState} from 'react'
+import Modal from "./Modal";
+import Backdrop from "./Backdrop";
 
 function Todo(props) {
-const [modalIsOpen, setModalIsOpen] = useState(false)
+    const [modalIsOpen, setModalIsOpen] = useState(false)
 
     function DeleteHandler() {
         setModalIsOpen(true)
     }
 
-    return (
-        <div className='card'>
-            <h2>{props.text}</h2>
-            <div className='actions'>
-                <button className='btn' onClick={DeleteHandler}>Delete</button>
+    return (<>
+            <div className='card'>
+                <h2>{props.text}</h2>
+                <div className='actions'>
+                    <button className='btn' onClick={DeleteHandler}>Delete</button>
+                </div>
             </div>
-        </div>
+        {modalIsOpen ? <Modal/> : ''}
+        {modalIsOpen ? <Backdrop/> : ''}
+        </>
+
     );
 }
 
